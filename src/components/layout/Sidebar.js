b304b0d0ea99670f5d9a90d5eca249e3f3a311cd
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2020-07-14 04:33:33 -0500

    Sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import user from '../../assets/icons/747376.svg';
+import clock from '../../assets/icons/149316.svg';
 
 const Sidebar = () => {
     return (
         <aside className="sidebar">
-            <h1>Sidebar</h1>
+            <div className="user">
+                <p>3</p>
+                <img src={user} alt="user"/>
+            </div>
+            <div className="order">
+                <h1>My Order</h1>
+                <div className="info">
+                    <div className="direction">
+                        <p>625 St Marks Ave</p>
+                        <button>Edit</button>
+                    </div>
+                    <div className="time">
+                        <div className="value">
+                            <div className="container-clock">
+                                <img src={clock} alt="clock"/>
+                            </div>
+                            <p>35 min</p>
+                        </div>
+                        <button>Choose time</button>
+                    </div>
+                </div>
+            </div>
         </aside>
     );
 };
