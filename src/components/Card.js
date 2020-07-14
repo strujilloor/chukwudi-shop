daa8b4c50dcf81403aa988ed43dc4d593de6d244
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2020-07-14 00:39:12 -0500

    Products

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,26 +2,27 @@ import React from 'react';
 import img from '../assets/images/pexels-photo-156114.jpeg';
 import star from '../assets/icons/149220.svg';
 
-const Card = () => {
+const Card = ({ product }) => {
+
+    const { name, qualification, time, price, image } = product;
+
     return (
         <div className="card">
             <div className="image-container">
-                <img src={ img } alt="product"/>
+                <img src={ image } alt="product"/>
                 <div>
-                    25-30 <span>min</span>
+                    { time }
                 </div>
             </div>
-            <p className="name">Bagel Story</p>
+            <p className="name">{ name }</p>
             <div className="details">
                 <div>
                     <img src={ star } alt="star"/>
                     <p>
-                        4,7
+                        { qualification }
                     </p>
                 </div>
-                <p>Deli</p>
-                <p>Bagels</p>
-                <p>$$</p>
+                <p>${ price } </p>
             </div>
         </div>
     );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
+import Products from "../products.json"
 
 const Cards = () => {
+
+    const [products, setProducts] = useState(Products);
+
     return (
         <div className="cards-container">
-            <Card/>
-            <Card/>
-            <Card/>
+            {
+                products.map(product => (
+                    <Card product={product}/>
+                ))
+            }
         </div>
     );
 };
