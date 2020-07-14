2d0790eb6cc6335071bec5c9f0f37bdc6e3fcfe9
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2020-07-14 00:37:56 -0500

    Products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Layout from './components/layout';
 import Cards from './components/Cards';
+import hamburger from './assets/icons/1046784.svg';
 
 function App() {
   return (
     <Layout>
-      <h1>Content</h1>
+      <h1 
+        style={{width: "90%", margin: "3rem auto"}}
+      >
+        Restaurants
+        <img 
+          src={hamburger} 
+          alt="hamburger"
+          style={{width: "2.5rem", marginLeft: "1.4rem"}}
+        />
+      </h1>
       <Cards/>
     </Layout>
   );
